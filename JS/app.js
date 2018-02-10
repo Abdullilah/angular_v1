fd911dac338ffd3c8ec60c82b8b204687faf6f8d
abdullilaph <abdullilaphd@hotmail.com>
2018-02-10 20:58:45 +0100

    Change order with click on the title

--- JS/app.js
+++ JS/app.js
@@ -5,10 +5,19 @@ app.controller('myController', function($scope, $http) {
         $scope.clients = response.data.clients;
         $scope.searchVlue = '';
         $scope.orderValue = '+name';
+        $scope.orderStatus = '+';
         $scope.slectedClient = null;
         $scope.clientClicked = function(row){
             $scope.slectedClient = row;
             console.log($scope.slectedClient);
+        };
+        $scope.changeOrder = function(orderName){
+            if($scope.orderStatus === '+'){
+                $scope.orderStatus = '-';
+            } else{
+                $scope.orderStatus = '+';
+            }
+            $scope.orderValue = $scope.orderStatus + orderName;
         }
     });
 });
